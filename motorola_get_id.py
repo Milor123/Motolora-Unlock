f46ec3dede2fb31deabbef50e9eea5295a4e8064
root <kora_bohorquez@hotmail.com>
2016-06-22 21:24:51 -0400

    Add error logs

--- motorola_get_id.py
+++ motorola_get_id.py
@@ -6,18 +6,21 @@ try:
     igg = 'fastboot oem get_unlock_data'
     p = Popen(igg, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
     output = p.stdout.read().splitlines()
+    code = []
+    for x in output:
+        if str(x[1:11]) == 'bootloader': # 1:11 is 'bootloader' > (bootloader) xxxCodexxx
+        code.append(x[13:]) # 13: is 'xxxCodexx'
+    alldata= ('').join(code)
+    print alldata
+    myfile = open('Code_Bootloader','w')
+    myfile.write(alldata)
+    myfile.close()
 
 except:
-    print 'Error Desconocido, Intenta ejecutar este archivo donde este el fastboot o agregalo a la consola'
+    message= 'Error Desconocido, Intenta ejecutar este archivo donde este el fastboot o agregalo a la consola'
+    myfile = open('motorola_get_id_Error.log','w')
+    myfile.write(message)
+    myfile.close()
     exit()
 
-code = []
-for x in output:
-    if str(x[1:11]) == 'bootloader': # 1:11 is 'bootloader' > (bootloader) xxxCodexxx
-       code.append(x[13:]) # 13: is 'xxxCodexx'
-alldata= ('').join(code)
-print alldata
-myfile = open('Code_Bootloader','w')
-myfile.write(alldata)
-myfile.close()
 
